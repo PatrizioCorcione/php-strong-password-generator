da275f20a3beabadeee36c632264109621d315ac
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-04-16 13:54:13 +0200

    [README]

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$letters = 'qwertyuiopasdfghjklzxcvbnm';
+$numbers = '1234567890';
+$simbols = '!?&%$<>^+-*/()[]{}@# _=';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php echo $simbols[1] ?>
+</body>
+
+</html>
